f033284a64c537b7fca6535c4f33cae65ffcabc1
karpom000 <karpom000@utp.edu.pl>
2023-11-29 19:45:41 +0100

    Update Ghost AI and more.

---
+++ Assets/Navigation/NavMeshBake.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshBake : MonoBehaviour
+{
+    public List<NavMeshSurface> surfaces;
+    private int size = 0;
+    private bool baked;
+
+    private void Awake()
+    {
+        if (surfaces == null)
+        {
+            surfaces = new List<NavMeshSurface>();
+            size = surfaces.Count;
+            baked = false;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!baked)
+        {
+            if (size == surfaces.Count)
+            {
+                Bake();
+                baked = true;
+            }
+            else
+            {
+                size = surfaces.Count;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!baked)
+        {
+            if (size == surfaces.Count)
+            {
+                Bake();
+                baked = true;
+            }
+            else
+            {
+                size = surfaces.Count;
+            }
+        }
+    }
+
+    void Bake()
+    {
+        for (int i = 0; i < surfaces.Count; i++)
+        {
+            //surfaces[i].ClearAllNavMeshes();
+            surfaces[i].BuildNavMesh();
+            Debug.Log("Bake_surfaces");
+        }
+    }
+}
--- Assets/engine.cs
+++ Assets/engine.cs
@@ -7,6 +7,9 @@ using UnityEngine.SceneManagement;
 public class engine : MonoBehaviour
 {
     public bool use_axe;
+    public Animator axeAnim;
+
+    private float axe_timer = 0.0f; //first animation delay
 
     public static int wood_boards = 0;
     public static int gasts = 0;
@@ -40,26 +43,34 @@ public class engine : MonoBehaviour
 
     public GameObject light;
 
+    private int item;
+
     public Camera cam;
 
     //dinamite
     public GameObject dinamite;
     public Transform dinamitePoint;
 
+    public AudioSource hide_gost;
 
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        info.text = "Woods: " + wood_boards + " Water: " + water + "Batteries: " + battery + "\nGasts: " + gasts;
+        info.text = "Woods: " + wood_boards + " Water: " + water + " Batteries: " + battery + "\nGasts: " + gasts;
         //power = 20;
         light.SetActive(false);
         //axe_rotate = axe.rotation;
+        use_axe = false;
+        item = 0;
+        axe_timer = 1.5f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        axe_timer -= Time.deltaTime;
+
         isGrounded = Physics.CheckSphere(ground.position, groundDistance, layerMask);
 
         if (isGrounded && vorlocity.y < 0)
@@ -176,15 +187,41 @@ public class engine : MonoBehaviour
                     
                 }
                 else
+                if (hitInfo.transform.gameObject.name == "chest")
+                {
+                    GameObject chest = hitInfo.transform.gameObject;
+                    ChestEngine chestEngine = chest.GetComponent<ChestEngine>();
+                    if (chestEngine.isOpen == true)
+                    {
+                        chestEngine.closeChest = true;                      
+                    }
+                    else
+                    {
+                        if (!chestEngine.isEmpty)
+                        {
+                            ChestEngine.ChestItems chestItems = chestEngine.chestItems;
+                            wood_boards += chestItems.wood;
+                            chestItems.wood = 0;
+                            water += chestItems.water;
+                            chestItems.water = 0;
+                            chestEngine.isEmpty = true;
+                            //info.text = "Woods: " + wood_boards + " Water: " + water + " Batteries: " + battery + "\nGasts: " + gasts;
+                        }
+                        chestEngine.openChest = true;
+                    }
+
+                }
+                else
                 if (hitInfo.transform.gameObject.name == "box_red")
                 {
                     CheckLevel.corridors += 2;
                     CheckLevel.rooms += 1;
                     CheckLevel.treasures = CheckLevel.levelId / 7;
                     CheckLevel.levelId++;
+                    //NavMeshBake.surfaces.Clear();
                     SceneManager.LoadScene(SceneManager.GetActiveScene().name);
                 }
-                //info.text = "Woods: " + wood_boards + " Water: " + water + "Batteries: " + battery + "\nGasts: " + gasts;
+                //info.text = "Woods: " + wood_boards + " Water: " + water + " Batteries: " + battery + "\nGasts: " + gasts;
             }
         }
 
@@ -197,30 +234,85 @@ public class engine : MonoBehaviour
                 water--;
                 wood_boards--;
 
-                info.text = "Woods: " + wood_boards + " Water: " + water + "Batteries: " + battery + "\nGasts: " + gasts;
+                info.text = "Woods: " + wood_boards + " Water: " + water + " Batteries: " + battery + "\nGasts: " + gasts;
             }
         }
 
         if (Input.GetKeyDown(KeyCode.E))
         {
-            if (light.activeSelf)
+            item++;
+            if (item < 3 && axe_timer <= 0.0f)
             {
-                light.SetActive(false);
-                book.gameObject.SetActive(true);
-                //axe.rotation = axe_rotate;
+                if (item == 1)
+                {
+                    light.SetActive(true);
+                    book.gameObject.SetActive(false);
+                    axeAnim.gameObject.SetActive(false);
+                }
+                else
+                if (item == 2)
+                {
+                    light.SetActive(false);
+                    book.gameObject.SetActive(true);
+                    axeAnim.gameObject.SetActive(false);
+                }
             }
             else
             {
-                light.SetActive(true);
-                /*light.GetComponent<Light>().enabled = true;
-                object halo = light.GetComponent("Halo");
-                var haloInfo = halo.GetType().GetProperty("enabled");
-                haloInfo.SetValue(halo, true, null);*/
+                item = 0;
+                light.SetActive(false);
                 book.gameObject.SetActive(false);
+                axeAnim.gameObject.SetActive(true);
+                axe_timer = 1.5f;
             }
-            //Debug.Log("Wood boards: " + wood_boards);
         }
 
-        info.text = "Woods: " + wood_boards + " Water: " + water + "Batteries: " + battery + "\nGasts: " + gasts;
+        if (Input.GetButtonDown("Fire1") && use_axe == false && axe_timer <= 0.0f)
+        {
+            axeAnim.Play("Base Layer.Blend Tree", 0, 0.0f);
+
+            RaycastHit hitInfo = new RaycastHit();
+            bool hit = Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hitInfo);
+            if (hit && hitInfo.distance < 1.75f)
+            {
+                Debug.Log("Hit attack: " + hitInfo.transform.parent.gameObject.name + " " + hitInfo.distance);
+                if (hitInfo.transform.parent.gameObject.name == "secret(Clone)")
+                {
+                    GameObject wall = hitInfo.transform.gameObject;
+                    wall.GetComponent<wall_health>().health -= 2;
+                }
+                else
+                if (hitInfo.transform.parent.gameObject.name == "gost" || hitInfo.transform.parent.gameObject.name == "gost(Clone)")
+                {
+                    GameObject tmp_gost = hitInfo.transform.gameObject;
+                    Debug.Log(tmp_gost.name);
+                    AxeGost axeGost = tmp_gost.transform.parent.Find("AttackSphere").GetComponent<AxeGost>();
+                    axeGost .health -= 4;
+                    axeGost.UpdateGost(tmp_gost.transform.parent.gameObject);
+                    /*hide_gost = tmp_gost.transform.parent.Find("AttackSphere").GetComponent<AudioSource>();
+                    Debug.Log(hide_gost.gameObject.name);
+                    hide_gost.Play();*/
+                }
+            }
+        }
+
+        info.text = "Woods: " + wood_boards + " Water: " + water + " Batteries: " + battery + "\nGasts: " + gasts;
+
+        //if (use_axe == true)
+        {
+            /*Debug.Log(axeAnim.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Blend Tree"));
+            Debug.Log("Time normal axe: " + axeAnim.GetCurrentAnimatorStateInfo(0).normalizedTime);
+            Debug.Log("Use axe: " + use_axe);*/
+
+            if (!(axeAnim.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Blend Tree") &&
+                axeAnim.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f))
+            {
+                use_axe = false;
+            }
+            else
+            {
+                use_axe = true;
+            }
+        }
     }
 }
