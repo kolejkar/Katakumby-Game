8b988cf86447e02d6192b5e8037fcc30d6189f26
karpom000 <karpom000@utp.edu.pl>
2024-05-11 23:13:18 +0200

    Add detail column and bug fixing monster moving.

--- Assets/GPS.cs
+++ Assets/GPS.cs
@@ -7,14 +7,10 @@ public class GPS : MonoBehaviour
 {
     public NavMeshAgent Agent;
 
-    public Transform trans;
     public GameObject type_room;
-    private bool new_room;
     // Start is called before the first frame update
     void Start()
     {
-        //transform.Rotate(Vector3.up * 180.0f);
-        //new_room = false;
         GameObject level = GameObject.Find("Level");
         if ((level == null))
         {
@@ -25,88 +21,9 @@ public class GPS : MonoBehaviour
         RandSection();
     }
 
-    void RandRotate()
-    {
-        Debug.Log(type_room.name);
-        if (type_room.name == "corossing")
-        {
-            if (type_room.transform.position.x - 0.2 < transform.position.x && type_room.transform.position.x + 0.2 > transform.position.x)
-            {
-                if (type_room.transform.position.z - 0.2 < transform.position.z && type_room.transform.position.z + 0.2 > transform.position.z)
-                {
-                    float id = Random.Range(0, 3);
-                    if (id == 0)
-                        trans.Rotate(Vector3.up * 180.0f);
-                    if (id == 1)
-                        trans.Rotate(Vector3.up * 90.0f);
-                    if (id == 2)
-                        trans.Rotate(Vector3.up * -90.0f);
-                    new_room = false;
-                }
-            }
-        }
-        else
-        if (type_room.name == "room_end")
-        {
-            //if (type_room.transform.position.x - 0.2 < transform.position.x && type_room.transform.position.x + 0.2 > transform.position.x)
-            {
-                //if (type_room.transform.position.z - 0.2 < transform.position.z && type_room.transform.position.z + 0.2 > transform.position.z)
-                {
-                    trans.Rotate(Vector3.up * 180.0f);
-                    new_room = false;
-                }
-            }
-        }
-        else
-        if (type_room.name == "corner")
-        {
-            if (type_room.transform.position.x - 0.2 < transform.position.x && type_room.transform.position.x + 0.2 > transform.position.x)
-            {
-                if (type_room.transform.position.z - 0.2 < transform.position.z && type_room.transform.position.z + 0.2 > transform.position.z)
-                {
-                    trans.Rotate(Vector3.up * 180.0f);
-                    new_room = false;
-                }
-            }
-        }
-        else
-        if (type_room.name == "T_crossing")
-        {
-            if (type_room.transform.position.x - 0.2 < transform.position.x && type_room.transform.position.x + 0.2 > transform.position.x)
-            {
-                if (type_room.transform.position.z - 0.2 < transform.position.z && type_room.transform.position.z + 0.2 > transform.position.z)
-                {
-                    trans.Rotate(Vector3.up * 180.0f);
-                    new_room = false;
-                }
-            }
-        }
-        else
-        if (type_room.name == "room_crossing")
-        {
-            if (type_room.transform.position.x - 0.2 < transform.position.x && type_room.transform.position.x + 0.2 > transform.position.x)
-            {
-                if (type_room.transform.position.z - 0.2 < transform.position.z && type_room.transform.position.z + 0.2 > transform.position.z)
-                {
-                    trans.Rotate(Vector3.up * 180.0f);
-                    new_room = false;
-                }
-            }
-        }
-        else
-        {
-            new_room = false;
-        }
-    }
-
     // Update is called once per frame
     void Update()
     {
-        /*if (new_room == true)
-        {
-            RandRotate();
-        }
-        trans.Translate(transform.forward * Time.deltaTime, Space.World);*/
         if (this.type_room.transform.position == this.floor.transform.position)
         {
             RandSection();
@@ -114,42 +31,14 @@ public class GPS : MonoBehaviour
 
     }
 
-    /*void OnCollisionEnter(Collision coll)
-    {
-        Debug.Log(coll.gameObject.name);
-        if (coll.gameObject.name == "floor" && new_room == false)
-        {
-            type_room = coll.gameObject.transform.parent.gameObject;
-            new_room = true;
-            return;
-        }
-        else
-        if (coll.gameObject.name == "end" && coll.gameObject.transform.parent.gameObject.name == "wall_end")
-        {
-            Debug.Log("end");
-            trans.Rotate(Vector3.up * -180.0f);
-            return;
-        }
-        //type_room = null;
-    }*/
 
     void OnTriggerEnter(Collider coll)
     {
         Debug.Log(coll.gameObject.name);
         if (coll.gameObject.name == "floor")
         {
-            type_room = coll.gameObject/*.transform.parent.gameObject*/;
-            //new_room = true;
-            //return;
+            type_room = coll.gameObject;
         }
-        /*else
-        if (coll.gameObject.name == "end" && coll.gameObject.transform.parent.gameObject.name == "wall_end")
-        {
-            Debug.Log("end");
-            trans.Rotate(Vector3.up * 180.0f);
-            return;
-        }*/
-        //type_room = null;
     }
 
     public int room_id = -1;
@@ -166,31 +55,36 @@ public class GPS : MonoBehaviour
         {
             room_id = Random.Range(0, items.Count);
             room_name_id = items[room_id].name;
-            if (room_name_id == "end_wall(Clone)")
+            if (room_name_id.IndexOf("end_wall") > -1)
             {
                 continue;
             }
             if (room_id != old_room_id)
             {
                 break;
-            }
+            } 
         }
 
         room_name_id = room_name_id.Replace("(Clone)", "");
+
+        if (room_name_id.IndexOf("room_spawn") > -1)
+        {
+            items[room_id] = FindInChildren(items[room_id], "room_end_spawn");
+        }
         
-        Debug.Log(room_name_id);
+        //Debug.Log(room_name_id);
 
         floor = FindInChildren(items[room_id], room_name_id);
 
-        Debug.Log(floor.name);
+        //Debug.Log(floor.name);
 
         floor = FindInChildren(floor, CheckSectionsName(room_name_id));
 
-        Debug.Log(floor.name);
+        //Debug.Log(floor.name);
 
         floor = FindInChildren(floor, "Floor");
 
-        Debug.Log(floor.name);
+        //Debug.Log(floor.name);
 
         Agent.SetDestination(floor.transform.position);
     }
@@ -223,7 +117,6 @@ public class GPS : MonoBehaviour
         {
             room_name = "corridor_" + room_name;
         }
-
         return room_name;
     }
 
