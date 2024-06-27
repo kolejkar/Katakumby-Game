0fa1888e4f35e181534b9aafe1ebd3d08fd14757
karpom000 <karpom000@utp.edu.pl>
2024-06-27 19:27:09 +0200

    Clean code in engine.cs script.

--- Assets/engine.cs
+++ Assets/engine.cs
@@ -77,8 +77,7 @@ public class engine : MonoBehaviour
 
     public GameObject floor;
 
-    // Update is called once per frame
-    void Update()
+    void MovePlayer()
     {
         isGrounded = Physics.CheckSphere(ground.position, groundDistance, layerMask);
 
@@ -120,7 +119,10 @@ public class engine : MonoBehaviour
         }
 
         characterController.Move(player.right * Input.GetAxis("Horizontal") * speedValue * Time.deltaTime + player.forward * Input.GetAxis("Vertical") * speedValue * Time.deltaTime);
+    }
 
+    void PlayerWalkSounds()
+    {
         if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
         {
             if ((!walk.isPlaying || walk_wood.isPlaying || walk_concrete.isPlaying) && walkDelay <= 0.0f)
@@ -142,218 +144,279 @@ public class engine : MonoBehaviour
             }
             //before_move = characterController.transform;
         }
+    }
+
+    void PlayerSetLight()
+    {
+        RaycastHit hitInfo = new RaycastHit();
+        bool hit = Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hitInfo);
+        if (hit && hitInfo.distance < 1.75f)
+        {
+            Debug.Log("Tourch hit p: " + hitInfo.transform.parent.gameObject.name + " " + hitInfo.distance);
+            if (hitInfo.transform.parent.gameObject.name == "candlle" || hitInfo.transform.parent.gameObject.name == "candlle (1)")
+            {
+                GameObject light = hitInfo.transform.parent.gameObject;
+                light.GetComponent<OpenLight>().SetLight();
+            }
+        }
+    }
+
+    void HitRock(RaycastHit hit)
+    {
+        GameObject wall = hit.transform.gameObject;
+        wall.GetComponent<wall_health>().health -= 2;
+    }
+
+    void HitGast(RaycastHit hit)
+    {
+        GameObject tmp_gost = hit.transform.gameObject;
+        Debug.Log(tmp_gost.name);
+        AxeGost axeGost = tmp_gost.transform.Find("AttackSphere").GetComponent<AxeGost>();
+        axeGost.health -= 4;
+        axeGost.UpdateGost(tmp_gost.transform.gameObject);
+        /*hide_gost = tmp_gost.transform.parent.Find("AttackSphere").GetComponent<AudioSource>();
+        Debug.Log(hide_gost.gameObject.name);
+        hide_gost.Play();*/
+    }
+
+    void PlayerUseAxe()
+    {
+        axeAnim.Play();
+
+        RaycastHit hitInfo = new RaycastHit();
+        bool hit = Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hitInfo);
+        Debug.Log("Hit attack: " + hitInfo.transform.gameObject.name + " " + hitInfo.distance);
+        if (hit && hitInfo.distance < 1.75f)
+        {
+            if (hitInfo.transform.parent.gameObject.name == "secret(Clone)")
+            {
+                HitRock(hitInfo);
+            }
+            else
+            if (hitInfo.transform.gameObject.name == "gast" || hitInfo.transform.gameObject.name == "gast(Clone)")
+            {
+                HitGast(hitInfo);
+            }
+        }
+    }
+
+    void ChangeActiveItem()
+    {
+        item++;
+        if (item < 3)
+        {
+            if (item == 1)
+            {
+                light.SetActive(true);
+                book.gameObject.SetActive(false);
+                axeAnim.gameObject.SetActive(false);
+            }
+            else
+            if (item == 2)
+            {
+                light.SetActive(false);
+                book.gameObject.SetActive(true);
+                axeAnim.gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            item = 0;
+            light.SetActive(false);
+            book.gameObject.SetActive(false);
+            axeAnim.gameObject.SetActive(true);
+        }
+    }
+
+    void SetDinamite()
+    {
+        if (wood_boards > 0 && water > 0)
+        {
+            Instantiate(dinamite, dinamitePoint.position, Quaternion.Euler(0.0f, 0.0f, 0.0f));
+            water--;
+            wood_boards--;
+
+            info.text = "Woods: " + wood_boards + " Water: " + water + " Batteries: " + battery + "\nGasts: " + gasts;
+        }
+    }
+
+    void RepairDoor(RaycastHit doorItem)
+    {
+        if (wood_boards >= 5)
+        {
+            GameObject breakdoor = doorItem.transform.parent.gameObject;
+            GameObject door = breakdoor.transform.parent.gameObject;
+            door.GetComponent<DoorPlacer>().RepairDoor();
+            Debug.Log(door.name);
+            wood_boards = wood_boards - 5;
+        }
+        else
+        {
+
+        }
+    }
+
+    void OpenCloceDoor(RaycastHit doorItem)
+    {
+        GameObject door = doorItem.transform.parent.gameObject;
+        if (door.GetComponent<DoorEngine>().isOpen == true)
+        {
+            door.GetComponent<DoorEngine>().closeDoor = true;
+        }
+        else
+        {
+            door.GetComponent<DoorEngine>().openDoor = true;
+        }
+    }
+
+    void AddItemsFromChest(ChestEngine chestEngine)
+    {
+        ChestEngine.ChestItems chestItems = chestEngine.chestItems;
+        wood_boards += chestItems.wood;
+        chestItems.wood = 0;
+        water += chestItems.water;
+        chestItems.water = 0;
+        chestEngine.isEmpty = true;
+        //info.text = "Woods: " + wood_boards + " Water: " + water + " Batteries: " + battery + "\nGasts: " + gasts;
+    }
+
+    void OpenCloseChest(RaycastHit chestItem)
+    {
+        GameObject chest = chestItem.transform.gameObject;
+        ChestEngine chestEngine = chest.GetComponent<ChestEngine>();
+        if (chestEngine.isOpen == true)
+        {
+            chestEngine.closeChest = true;
+        }
+        else
+        {
+            if (!chestEngine.isEmpty)
+            {
+                AddItemsFromChest(chestEngine);
+            }
+            chestEngine.openChest = true;
+        }
+    }
+
+    void InteractiveItems()
+    {
+        RaycastHit hitInfo = new RaycastHit();
+        bool hit = Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hitInfo);
+        if (hit && hitInfo.distance < 1.75f)
+        {
+            Debug.Log("Hit " + hitInfo.transform.gameObject.name + " " + hitInfo.distance);
+            if (hitInfo.transform.gameObject.name == "BreakWoodDoor")
+            {
+                RepairDoor(hitInfo);
+            }
+            else
+            if (hitInfo.transform.gameObject.name == "wood")
+            {
+                Destroy(hitInfo.collider.gameObject);
+                wood_boards++;
+            }
+            else
+            if (hitInfo.transform.gameObject.name == "Battery")
+            {
+                Destroy(hitInfo.collider.gameObject);
+                battery++;
+            }
+            else
+            if (hitInfo.transform.gameObject.name == "bottle")
+            {
+                Destroy(hitInfo.collider.gameObject);
+                water++;
+            }
+            else
+            if (hitInfo.transform.gameObject.name == "bridge_destroyed")
+            {
+                if (wood_boards >= 10)
+                {
+                    GameObject bridge2 = hitInfo.transform.parent.gameObject;
+                    GameObject lvl2 = bridge2.transform.parent.gameObject;
+                    Destroy(bridge2);
+                    lvl2.GetComponent<bridge_generator>().MakeBridge();
+                    Debug.Log(lvl2.name);
+                    wood_boards = wood_boards - 10;
+                }
+                else
+                {
+
+                }
+            }
+            else
+            if (hitInfo.transform.gameObject.name.Contains("Stair"))
+            {
+                if (wood_boards >= 15)
+                {
+                    GameObject stairs2 = hitInfo.transform.parent.gameObject;
+                    GameObject lvl2 = stairs2.transform.parent.gameObject;
+                    Destroy(stairs2);
+                    lvl2.GetComponent<bridge_generator>().MakeStairs();
+                    Debug.Log(lvl2.name);
+                    wood_boards = wood_boards - 15;
+                }
+                else
+                {
+
+                }
+            }
+            else
+            if (hitInfo.transform.gameObject.name == "WoodDoor")
+            {
+                OpenCloceDoor(hitInfo);
+            }
+            else
+            if (hitInfo.transform.gameObject.name == "chest(Clone)")
+            {
+                OpenCloseChest(hitInfo);
+            }
+            else
+            if (hitInfo.transform.gameObject.name == "box_red")
+            {
+
+            }
+            //info.text = "Woods: " + wood_boards + " Water: " + water + " Batteries: " + battery + "\nGasts: " + gasts;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+
+        PlayerWalkSounds();
 
         vorlocity.y += gravitiy * Time.deltaTime;
         characterController.Move(vorlocity * Time.deltaTime);
 
         if (Input.GetButtonDown("Fire2"))
         {
-            RaycastHit hitInfo = new RaycastHit();
-            bool hit = Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hitInfo);
-            if (hit && hitInfo.distance < 1.75f)
-            {
-                Debug.Log("Hit " + hitInfo.transform.gameObject.name + " " + hitInfo.distance);
-                if (hitInfo.transform.gameObject.name == "BreakWoodDoor")
-                {
-                    if (wood_boards >= 5)
-                    {
-                        GameObject breakdoor = hitInfo.transform.parent.gameObject;
-                        GameObject door = breakdoor.transform.parent.gameObject;
-                        door.GetComponent<DoorPlacer>().RepairDoor();
-                        Debug.Log(door.name);
-                        wood_boards = wood_boards - 5;
-                    }
-                    else
-                    {
-
-                    }
-                }
-                else
-                if (hitInfo.transform.gameObject.name == "wood")
-                {
-                    Destroy(hitInfo.collider.gameObject);
-                    wood_boards++;
-                }
-                else
-                if (hitInfo.transform.gameObject.name == "Battery")
-                {
-                    Destroy(hitInfo.collider.gameObject);
-                    battery++;
-                }
-                else
-                if (hitInfo.transform.gameObject.name == "bottle")
-                {
-                    Destroy(hitInfo.collider.gameObject);
-                    water++;
-                }
-                else
-                if (hitInfo.transform.gameObject.name == "bridge_destroyed")
-                {
-                    if (wood_boards >= 10)
-                    {
-                        GameObject bridge2 = hitInfo.transform.parent.gameObject;
-                        GameObject lvl2 = bridge2.transform.parent.gameObject;
-                        Destroy(bridge2);
-                        lvl2.GetComponent<bridge_generator>().MakeBridge();
-                        Debug.Log(lvl2.name);
-                        wood_boards = wood_boards - 10;
-                    }
-                    else
-                    {
-
-                    }
-                }
-                else
-                if (hitInfo.transform.gameObject.name.Contains("Stair"))
-                {
-                    if (wood_boards >= 15)
-                    {
-                        GameObject stairs2= hitInfo.transform.parent.gameObject;
-                        GameObject lvl2 = stairs2.transform.parent.gameObject;
-                        Destroy(stairs2);
-                        lvl2.GetComponent<bridge_generator>().MakeStairs();
-                        Debug.Log(lvl2.name);
-                        wood_boards = wood_boards - 15;
-                    }
-                    else
-                    {
-
-                    }
-                }
-                else
-                if (hitInfo.transform.gameObject.name == "WoodDoor")
-                {
-                    GameObject door = hitInfo.transform.parent.gameObject;
-                    if (door.GetComponent<DoorEngine>().isOpen == true)
-                    {
-                        door.GetComponent<DoorEngine>().closeDoor = true;
-                    }
-                    else
-                    {
-                        door.GetComponent<DoorEngine>().openDoor = true;
-                    }
-                    
-                }
-                else
-                if (hitInfo.transform.gameObject.name == "chest(Clone)")
-                {
-                    GameObject chest = hitInfo.transform.gameObject;
-                    ChestEngine chestEngine = chest.GetComponent<ChestEngine>();
-                    if (chestEngine.isOpen == true)
-                    {
-                        chestEngine.closeChest = true;                      
-                    }
-                    else
-                    {
-                        if (!chestEngine.isEmpty)
-                        {
-                            ChestEngine.ChestItems chestItems = chestEngine.chestItems;
-                            wood_boards += chestItems.wood;
-                            chestItems.wood = 0;
-                            water += chestItems.water;
-                            chestItems.water = 0;
-                            chestEngine.isEmpty = true;
-                            //info.text = "Woods: " + wood_boards + " Water: " + water + " Batteries: " + battery + "\nGasts: " + gasts;
-                        }
-                        chestEngine.openChest = true;
-                    }
-
-                }
-                else
-                if (hitInfo.transform.gameObject.name == "box_red")
-                {
-                   
-                }
-                //info.text = "Woods: " + wood_boards + " Water: " + water + " Batteries: " + battery + "\nGasts: " + gasts;
-            }
+            InteractiveItems();
         }
 
         if (Input.GetKeyDown(KeyCode.Q))
         {
             //set dinamite
-            if (wood_boards > 0 && water > 0)
-            {
-                Instantiate(dinamite, dinamitePoint.position, Quaternion.Euler(0.0f, 0.0f, 0.0f));
-                water--;
-                wood_boards--;
-
-                info.text = "Woods: " + wood_boards + " Water: " + water + " Batteries: " + battery + "\nGasts: " + gasts;
-            }
+            SetDinamite();
         }
 
         if (Input.GetKeyDown(KeyCode.E) && !use_axe)
         {
-            item++;
-            if (item < 3)
-            {
-                if (item == 1)
-                {
-                    light.SetActive(true);
-                    book.gameObject.SetActive(false);
-                    axeAnim.gameObject.SetActive(false);
-                }
-                else
-                if (item == 2)
-                {
-                    light.SetActive(false);
-                    book.gameObject.SetActive(true);
-                    axeAnim.gameObject.SetActive(false);
-                }
-            }
-            else
-            {
-                item = 0;
-                light.SetActive(false);
-                book.gameObject.SetActive(false);
-                axeAnim.gameObject.SetActive(true);
-            }
+            ChangeActiveItem();
         }
 
         if (Input.GetButtonDown("Fire1") && use_axe == false && axeAnim.gameObject.activeSelf)
         {
-            axeAnim.Play();
-
-            RaycastHit hitInfo = new RaycastHit();
-            bool hit = Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hitInfo);
-            Debug.Log("Hit attack: " + hitInfo.transform.gameObject.name + " " + hitInfo.distance);
-            if (hit && hitInfo.distance < 1.75f)
-            {
-                if (hitInfo.transform.parent.gameObject.name == "secret(Clone)")
-                {
-                    GameObject wall = hitInfo.transform.gameObject;
-                    wall.GetComponent<wall_health>().health -= 2;
-                }
-                else
-                if (hitInfo.transform.gameObject.name == "gast" || hitInfo.transform.gameObject.name == "gast(Clone)")
-                {
-                    GameObject tmp_gost = hitInfo.transform.gameObject;
-                    Debug.Log(tmp_gost.name);
-                    AxeGost axeGost = tmp_gost.transform.Find("AttackSphere").GetComponent<AxeGost>();
-                    axeGost.health -= 4;
-                    axeGost.UpdateGost(tmp_gost.transform.gameObject);
-                    /*hide_gost = tmp_gost.transform.parent.Find("AttackSphere").GetComponent<AudioSource>();
-                    Debug.Log(hide_gost.gameObject.name);
-                    hide_gost.Play();*/
-                }
-            }
+            PlayerUseAxe();
         }
         if (Input.GetButtonDown("Fire1") && light.activeSelf)
         {
-            RaycastHit hitInfo = new RaycastHit();
-            bool hit = Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hitInfo);
-            if (hit && hitInfo.distance < 1.75f)
-            {
-                Debug.Log("Tourch hit p: " + hitInfo.transform.parent.gameObject.name + " " + hitInfo.distance);
-                if (hitInfo.transform.parent.gameObject.name == "candlle" || hitInfo.transform.parent.gameObject.name == "candlle (1)")
-                {
-                    GameObject light = hitInfo.transform.parent.gameObject;
-                    light.GetComponent<OpenLight>().SetLight();
-                }
-            }
+            PlayerSetLight();
         }
         if (Input.GetButtonDown("Cancel"))
         {
-            
+            SceneManager.LoadScene("Menu");
         }
 
         info.text = "Woods: " + wood_boards + " Water: " + water + " Batteries: " + battery + "\nGasts: " + gasts;
